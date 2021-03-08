10d749e91d0ea719a6f12cfe6dcc8fa31059f620
Ali Almazedi <ali.almazedi@boothamstudent.co.uk>
2021-03-08 15:38:35 +0000

    o

--- server.js
+++ server.js
@@ -37,9 +37,9 @@ io.on("connection", socket => {
 });
 
 if (process.env.PROD) {
-    app.use(express.static(path.join(_dirname, '.client/build')));
+    app.use(express.static(path.join(__dirname, '.client/build')));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(_dirname, './client/build/index.html'));
+        res.sendFile(path.join(__dirname, './client/build/index.html'));
     });
 }
 
