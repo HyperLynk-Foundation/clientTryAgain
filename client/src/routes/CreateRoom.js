1aad9936f27183bfad1b515bec90c25597fe6b1d
Naman P <66672786+HyperLynk@users.noreply.github.com>
2021-05-05 08:28:57 +0100

    Update CreateRoom.js

--- client/src/routes/CreateRoom.js
+++ client/src/routes/CreateRoom.js
@@ -10,7 +10,7 @@ const CreateRoom = (props) => {
     return (
         <div className={"container"}>
             <div className={"header"}>
-                <img src="https://lh4.googleusercontent.com/v18lF9r6zxRLfMfUGViavlD5bBa1gztym8W0QFNUKOuiC8YiD1enL0ecfZ-EL7zqQN-hNR9bMJUQhFJ0KtZ3cRakmF9de4h0CzZJJWtP5m0C3asiUHKxBkYbJdDqXbDM5wx9mlx3byUWOGZvbxkCPkVzpK3gPNI07WfKESABwM7513BRBxCiSs_SCs-hkD7_ra6Daf04A0lckvH2ptHGIAcKKvYou9g=w521" alt="Girl in a jacket" width="200"/>
+                <img src="https://lh6.googleusercontent.com/hQfk_x_Ga4Ruj7szlSJQrv-NIu2eScEpXEXc6061LGpTghDT1CIXXWkzQfKN6knvElQNJ_5By_Em26cO8JgIJpoDdaaCGMLCfIydn3DnyCkJu41i3mqTPyh8EutIZPiybbiYSVRjBOax45nLtONOibJhhTO_fkiqAQTspRUP-SEMOHhw2QDLlGpqPVCewy9sVgnirZ2r3sh0gmw5s07efUdNi-2rPes=w1920-h966" alt="Handshake Logo" width="200"/>
                 <h2>Fast, Capable, Private</h2>
             </div>
             <div className={"main2"}>
