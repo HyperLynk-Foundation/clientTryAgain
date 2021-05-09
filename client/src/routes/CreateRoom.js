aa9ee42c4935574098701fbe6f0ce08e5ff874f3
Naman P <66672786+HyperLynk@users.noreply.github.com>
2021-05-09 20:10:19 +0100

    Update CreateRoom.js

--- client/src/routes/CreateRoom.js
+++ client/src/routes/CreateRoom.js
@@ -10,7 +10,7 @@ const CreateRoom = (props) => {
     return (
         <div className={"container"}>
             <div className={"header"}>
-                <img src="https://lh6.googleusercontent.com/hQfk_x_Ga4Ruj7szlSJQrv-NIu2eScEpXEXc6061LGpTghDT1CIXXWkzQfKN6knvElQNJ_5By_Em26cO8JgIJpoDdaaCGMLCfIydn3DnyCkJu41i3mqTPyh8EutIZPiybbiYSVRjBOax45nLtONOibJhhTO_fkiqAQTspRUP-SEMOHhw2QDLlGpqPVCewy9sVgnirZ2r3sh0gmw5s07efUdNi-2rPes=w1920-h966" alt="Handshake Logo" width="200"/>
+                <img src="https://i.ibb.co/nr3vGdN/handshakelogo.png" alt="Handshake Logo" width="200"/>
                 <h2>Fast, Capable, Private</h2>
             </div>
             <div className={"main2"}>
